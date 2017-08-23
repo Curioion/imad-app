a6aab78cd113a4387f45276186ef73d17d8fd3f1
Curioion <30831765+Curioion@users.noreply.github.com>
2017-08-23 22:23:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ var htmlTemplate = `
      <link href="/ui/style.css" rel="stylesheet" />
     </head>
     <body>
-       <div class = 'container'>
+       <div class = 'container', id = 'maintext'>
            <div>
          <a href = '/'>Home</a>
           </div> 
