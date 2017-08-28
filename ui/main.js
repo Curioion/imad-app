6aaf874a5d1232932cbf8137b3d36215c88e38c4
Curioion <30831765+Curioion@users.noreply.github.com>
2017-08-28 12:33:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ var img = document.getElementById('leaf');
 var marginLeft = 0;
 function moveRight ()
 {
-    marginLeft = 10;
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft+'px';
 }
 
