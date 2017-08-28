3e06b6436d7125bcad294935f30a4a3be6925e88
Curioion <30831765+Curioion@users.noreply.github.com>
2017-08-28 16:09:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ button.onclick = function(){
      if(request.status === 200 )
      {
          var names = request.responseText;
-         names = JSON.parse(names)
+         names = JSON.parse(names);
         var list = '';
       for(var i = 0; i< names.lenght;i++)
        {
