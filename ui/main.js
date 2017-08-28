ad148159ed7db58e5fa5f04d4a79b445c2cd0782
Curioion <30831765+Curioion@users.noreply.github.com>
2017-08-28 16:08:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,43 +1,51 @@
-// Counter Button Code
 var button = document.getElementById('counter');
-
-button.onclick = function () {
-    
-    //Create request Object
-    var request = new XMLHttpRequest();
-    
-    //capture the request
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE)
-        {
-            if(request.status === 200 )
-            {
-               var counter = request.responseText;
-                var span = document.getElementById('count');
-               span.innerHTML = counter.toString();
-            }
-        }
-    };
-    //Make a request
-    request.open('GET','http://nvdt13513.imad.hasura-app.io/counter', true);
+var counter = 0;
+button.onclick = function(){
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+     if(request.status === 200 )
+     {
+         var counter = request.responseText;
+          var span = document.getElementById('count');
+         span.innerHTML = counter.toString();
+     }
+    }
+    request.open('GET', 'http://nvdt13513.imad.hasura-app.io/counter',true);
     request.send(null);
-    
 };
-//submit input code
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    var submit = document.getElementById('submit_btn');
-    submit.onclick = function(){
-        //Make a request ot the server and send the name
-        
-        //Capture the list of names and render it as a list.
-        var names = ['Name1','Name2','Name3','Name4','Name5'];
+
+button.onclick = function(){
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+     if(request.status === 200 )
+     {
+         var names = request.responseText;
+         names = JSON.parse(names)
         var list = '';
-        for(var i = 0; i<names.length; i++)
-        {
-            list += '<li>' + names[i] + '</li>';
-        }
-        var ul =document.getElementById('nameList');
-        ul.innerHTML = list;
-    };
-    
+      for(var i = 0; i< names.lenght;i++)
+       {
+         list += '<li>' + names[i] + '</li>';
+     }
+   var ul = document.getElementById('nameList');
+   ul.innerHTML = list;
+         
+     }
+    }
+    request.open('GET', 'http://nvdt13513.imad.hasura-app.io/submit-name?name=',true);
+    request.send(null);
+};
+
+//SUBMIT NAME
+ var nameInput = document.getElementById('name');
+ var name = nameInput.value;
+ var submit = document.getElementById('submit_btn');
+ submit.onclick= function(){
+     var names = ['name1','name2','name3','name4','name5'];
+     var list = '';
+     for(var i = 0; i< names.lenght;i++)
+     {
+         list += '<li>' + names[i] + '</li>';
+     }
+   var ul = document.getElementById('nameList');
+   ul.innerHTML = list;
+ };
