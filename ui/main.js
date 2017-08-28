b6612c042935f6a422b716e3e0412c5899cd349c
Curioion <30831765+Curioion@users.noreply.github.com>
2017-08-28 12:19:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,14 @@
 console.log('Loaded!');
 
+// Change the text
 var element = document.getElementById('james');
 
 element.innerHTML = "Changed and New";
+
+//move the image..
+
+var img = document.getElementById('leaf');
+element.onclick = function()
+{
+    img.style.margiLeft = '100px';
+}; 
